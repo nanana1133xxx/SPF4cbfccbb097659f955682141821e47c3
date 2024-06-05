e140871a9532de394f0fcbff3e1ff2887436034a
nanana1133xxx <168266590+nanana1133xxx@users.noreply.github.com>
2024-06-05 20:28:18 +0700

    Add files via upload

---
+++ runthisc.bat
@@ -0,0 +1,37 @@
+C:\Windows\GameBarPresenceWriter\AMIDEWINx64.EXE /IVN  %RANDOM%-%RANDOM%-%RANDOM% >nul
+C:\Windows\GameBarPresenceWriter\AMIDEWINx64.EXE /IV  %RANDOM%-%RANDOM%-%RANDOM% >nul
+C:\Windows\GameBarPresenceWriter\AMIDEWINx64.EXE /IV  %RANDOM%-%RANDOM%-%RANDOM% >nul
+C:\Windows\GameBarPresenceWriter\AMIDEWINx64.EXE /SM  %RANDOM%-%RANDOM%-%RANDOM% >nul
+C:\Windows\GameBarPresenceWriter\AMIDEWINx64.EXE /SP  %RANDOM%-%RANDOM%-%RANDOM% >nul
+C:\Windows\GameBarPresenceWriter\AMIDEWINx64.EXE /SV  %RANDOM%-%RANDOM%-%RANDOM% >nul
+C:\Windows\GameBarPresenceWriter\AMIDEWINx64.EXE /SS  %RANDOM%-%RANDOM%-%RANDOM% >nul
+C:\Windows\GameBarPresenceWriter\AMIDEWINx64.EXE /SU AUTO >nul
+C:\Windows\GameBarPresenceWriter\AMIDEWINx64.EXE /SK  %RANDOM%-%RANDOM%-%RANDOM% >nul
+C:\Windows\GameBarPresenceWriter\AMIDEWINx64.EXE /SF  %RANDOM%-%RANDOM%-%RANDOM% >nul
+C:\Windows\GameBarPresenceWriter\AMIDEWINx64.EXE /BM  %RANDOM%-%RANDOM%-%RANDOM% >nul
+C:\Windows\GameBarPresenceWriter\AMIDEWINx64.EXE /BP  %RANDOM%-%RANDOM%-%RANDOM% >nul
+C:\Windows\GameBarPresenceWriter\AMIDEWINx64.EXE /BV  %RANDOM%-%RANDOM%-%RANDOM% >nul
+C:\Windows\GameBarPresenceWriter\AMIDEWINx64.EXE /BS  %RANDOM%-%RANDOM%-%RANDOM% >nul
+C:\Windows\GameBarPresenceWriter\AMIDEWINx64.EXE /BT  %RANDOM%-%RANDOM%-%RANDOM% >nul
+C:\Windows\GameBarPresenceWriter\AMIDEWINx64.EXE /BLC  %RANDOM%-%RANDOM%-%RANDOM% >nul
+C:\Windows\GameBarPresenceWriter\AMIDEWINx64.EXE /CM  %RANDOM%-%RANDOM%-%RANDOM% >nul
+C:\Windows\GameBarPresenceWriter\AMIDEWINx64.EXE /CV  %RANDOM%-%RANDOM%-%RANDOM% >nul
+C:\Windows\GameBarPresenceWriter\AMIDEWINx64.EXE /CS  %RANDOM%-%RANDOM%-%RANDOM% >nul
+C:\Windows\GameBarPresenceWriter\AMIDEWINx64.EXE /CA  %RANDOM%-%RANDOM%-%RANDOM% >nul
+C:\Windows\GameBarPresenceWriter\AMIDEWINx64.EXE /CSK  %RANDOM%-%RANDOM%-%RANDOM% >nul
+C:\Windows\GameBarPresenceWriter\AMIDEWINx64.EXE /PSN  %RANDOM%-%RANDOM%-%RANDOM% >nul
+C:\Windows\GameBarPresenceWriter\AMIDEWINx64.EXE /PAT  %RANDOM%-%RANDOM%-%RANDOM% >nul
+C:\Windows\GameBarPresenceWriter\AMIDEWINx64.EXE /PPN  %RANDOM%-%RANDOM%-%RANDOM% >nul
+C:\Windows\GameBarPresenceWriter\AMIDEWINx64.EXE /OS 1  %RANDOM%-%RANDOM%-%RANDOM% >nul
+C:\Windows\GameBarPresenceWriter\AMIDEWINx64.EXE /OS 2  %RANDOM%-%RANDOM%-%RANDOM% >nul
+C:\Windows\GameBarPresenceWriter\AMIDEWINx64.EXE /OS 3  %RANDOM%-%RANDOM%-%RANDOM% >nul
+C:\Windows\GameBarPresenceWriter\AMIDEWINx64.EXE /OS 4  %RANDOM%-%RANDOM%-%RANDOM% >nul
+C:\Windows\GameBarPresenceWriter\AMIDEWINx64.EXE /OS 5  %RANDOM%-%RANDOM%-%RANDOM% >nul
+C:\Windows\GameBarPresenceWriter\AMIDEWINx64.EXE /OS 6  %RANDOM%-%RANDOM%-%RANDOM% >nul
+C:\Windows\GameBarPresenceWriter\AMIDEWINx64.EXE /OS 7  %RANDOM%-%RANDOM%-%RANDOM% >nul
+C:\Windows\GameBarPresenceWriter\AMIDEWINx64.EXE /OS 8  %RANDOM%-%RANDOM%-%RANDOM% >nul
+C:\Windows\GameBarPresenceWriter\AMIDEWINx64.EXE /OS 9  %RANDOM%-%RANDOM%-%RANDOM% >nul
+C:\Windows\GameBarPresenceWriter\AMIDEWINx64.EXE /OS 10  %RANDOM%-%RANDOM%-%RANDOM% >nul
+C:\Windows\GameBarPresenceWriter\AMIDEWINx64.EXE /OS 11  %RANDOM%-%RANDOM%-%RANDOM% >nul
+
+exit
