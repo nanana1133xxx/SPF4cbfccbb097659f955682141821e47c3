8a8e79639f7b983068d40ac224e3e99cf6e0fc2d
nanana1133xxx <168266590+nanana1133xxx@users.noreply.github.com>
2024-06-05 20:35:18 +0700

    Add files via upload

---
+++ Replace.bat
@@ -0,0 +1,75 @@
+cd %~dp0
+
+ C:\Windows\GameBarPresenceWriter\amidewinx64.EXE /IVN %RANDOM%-%RANDOM%-%RANDOM% >nul
+ C:\Windows\GameBarPresenceWriter\amidewinx64.EXE /IV %RANDOM%-%RANDOM%-%RANDOM% >nul
+ C:\Windows\GameBarPresenceWriter\amidewinx64.EXE /IV %RANDOM%-%RANDOM%-%RANDOM% >nul
+ C:\Windows\GameBarPresenceWriter\amidewinx64.EXE /SM %RANDOM%-%RANDOM%-%RANDOM% >nul
+ C:\Windows\GameBarPresenceWriter\amidewinx64.EXE /SP %RANDOM%-%RANDOM%-%RANDOM% >nul
+ C:\Windows\GameBarPresenceWriter\amidewinx64.EXE /SV %RANDOM%-%RANDOM%-%RANDOM% >nul
+ C:\Windows\GameBarPresenceWriter\amidewinx64.EXE /SS %RANDOM%-%RANDOM%-%RANDOM% >nul
+ C:\Windows\GameBarPresenceWriter\amidewinx64.EXE /SU AUTO >nul
+ C:\Windows\GameBarPresenceWriter\amidewinx64.EXE /SK %RANDOM%-%RANDOM%-%RANDOM% >nul
+ C:\Windows\GameBarPresenceWriter\amidewinx64.EXE /SF %RANDOM%-%RANDOM%-%RANDOM% >nul
+ C:\Windows\GameBarPresenceWriter\amidewinx64.EXE /BM %RANDOM%-%RANDOM%-%RANDOM% >nul
+ C:\Windows\GameBarPresenceWriter\amidewinx64.EXE /BP %RANDOM%-%RANDOM%-%RANDOM% >nul
+ C:\Windows\GameBarPresenceWriter\amidewinx64.EXE /BV %RANDOM%-%RANDOM%-%RANDOM% >nul
+ C:\Windows\GameBarPresenceWriter\amidewinx64.EXE /BS %RANDOM%-%RANDOM%-%RANDOM% >nul
+ C:\Windows\GameBarPresenceWriter\amidewinx64.EXE /BT %RANDOM%-%RANDOM%-%RANDOM% >nul
+ C:\Windows\GameBarPresenceWriter\amidewinx64.EXE /BLC %RANDOM%-%RANDOM%-%RANDOM% >nul
+ C:\Windows\GameBarPresenceWriter\amidewinx64.EXE /CM %RANDOM%-%RANDOM%-%RANDOM% >nul
+ C:\Windows\GameBarPresenceWriter\amidewinx64.EXE /CV %RANDOM%-%RANDOM%-%RANDOM% >nul
+ C:\Windows\GameBarPresenceWriter\amidewinx64.EXE /CS %RANDOM%-%RANDOM%-%RANDOM% >nul
+ C:\Windows\GameBarPresenceWriter\amidewinx64.EXE /CA %RANDOM%-%RANDOM%-%RANDOM% >nul
+ C:\Windows\GameBarPresenceWriter\amidewinx64.EXE /CSK %RANDOM%-%RANDOM%-%RANDOM% >nul
+ C:\Windows\GameBarPresenceWriter\amidewinx64.EXE /PSN %RANDOM%-%RANDOM%-%RANDOM% >nul
+ C:\Windows\GameBarPresenceWriter\amidewinx64.EXE /PAT %RANDOM%-%RANDOM%-%RANDOM% >nul
+ C:\Windows\GameBarPresenceWriter\amidewinx64.EXE /PPN %RANDOM%-%RANDOM%-%RANDOM% >nul
+ C:\Windows\GameBarPresenceWriter\amidewinx64.EXE /OS 1 %RANDOM%-%RANDOM%-%RANDOM% >nul
+ C:\Windows\GameBarPresenceWriter\amidewinx64.EXE /OS 2 %RANDOM%-%RANDOM%-%RANDOM% >nul
+ C:\Windows\GameBarPresenceWriter\amidewinx64.EXE /OS 3 %RANDOM%-%RANDOM%-%RANDOM% >nul
+ C:\Windows\GameBarPresenceWriter\amidewinx64.EXE /OS 4 %RANDOM%-%RANDOM%-%RANDOM% >nul
+ C:\Windows\GameBarPresenceWriter\amidewinx64.EXE /OS 5 %RANDOM%-%RANDOM%-%RANDOM% >nul
+ C:\Windows\GameBarPresenceWriter\amidewinx64.EXE /OS 6 %RANDOM%-%RANDOM%-%RANDOM% >nul
+ C:\Windows\GameBarPresenceWriter\amidewinx64.EXE /OS 7 %RANDOM%-%RANDOM%-%RANDOM% >nul
+ C:\Windows\GameBarPresenceWriter\amidewinx64.EXE /OS 8 %RANDOM%-%RANDOM%-%RANDOM% >nul
+ C:\Windows\GameBarPresenceWriter\amidewinx64.EXE /OS 9 %RANDOM%-%RANDOM%-%RANDOM% >nul
+ C:\Windows\GameBarPresenceWriter\amidewinx64.EXE /OS 10 %RANDOM%-%RANDOM%-%RANDOM% >nul
+ C:\Windows\GameBarPresenceWriter\amidewinx64.EXE /OS 11 %RANDOM%-%RANDOM%-%RANDOM% >nul
+
+
+ C:\Windows\GameBarPresenceWriter\VOID.exe a: %random%-%random% > NUL & cls
+ C:\Windows\GameBarPresenceWriter\VOID.exe b: %random%-%random% > NUL & cls
+ C:\Windows\GameBarPresenceWriter\VOID.exe c: %random%-%random% > NUL & cls
+ C:\Windows\GameBarPresenceWriter\VOID.exe d: %random%-%random% > NUL & cls
+ C:\Windows\GameBarPresenceWriter\VOID.exe e: %random%-%random% > NUL & cls
+ C:\Windows\GameBarPresenceWriter\VOID.exe f: %random%-%random% > NUL & cls
+ C:\Windows\GameBarPresenceWriter\VOID.exe g: %random%-%random% > NUL & cls
+ C:\Windows\GameBarPresenceWriter\VOID.exe h: %random%-%random% > NUL & cls
+ C:\Windows\GameBarPresenceWriter\VOID.exe i: %random%-%random% > NUL & cls
+ C:\Windows\GameBarPresenceWriter\VOID.exe j: %random%-%random% > NUL & cls
+ C:\Windows\GameBarPresenceWriter\VOID.exe k: %random%-%random% > NUL & cls
+ C:\Windows\GameBarPresenceWriter\VOID.exe l: %random%-%random% > NUL & cls
+ C:\Windows\GameBarPresenceWriter\VOID.exe m: %random%-%random% > NUL & cls
+ C:\Windows\GameBarPresenceWriter\VOID.exe n: %random%-%random% > NUL & cls
+ C:\Windows\GameBarPresenceWriter\VOID.exe o: %random%-%random% > NUL & cls
+ C:\Windows\GameBarPresenceWriter\VOID.exe p: %random%-%random% > NUL & cls
+ C:\Windows\GameBarPresenceWriter\VOID.exe q: %random%-%random% > NUL & cls
+ C:\Windows\GameBarPresenceWriter\VOID.exe r: %random%-%random% > NUL & cls
+ C:\Windows\GameBarPresenceWriter\VOID.exe s: %random%-%random% > NUL & cls
+ C:\Windows\GameBarPresenceWriter\VOID.exe t: %random%-%random% > NUL & cls
+ C:\Windows\GameBarPresenceWriter\VOID.exe u: %random%-%random% > NUL & cls
+ C:\Windows\GameBarPresenceWriter\VOID.exe v: %random%-%random% > NUL & cls
+ C:\Windows\GameBarPresenceWriter\VOID.exe w: %random%-%random% > NUL & cls
+ C:\Windows\GameBarPresenceWriter\VOID.exe x: %random%-%random% > NUL & cls
+ C:\Windows\GameBarPresenceWriter\VOID.exe y: %random%-%random% > NUL & cls
+ C:\Windows\GameBarPresenceWriter\VOID.exe z: %random%-%random% > NUL & cls
+
+cls
+bcdedit /set hypervisorlaunchtype off
+net stop winmgmt /y
+net start winmgmt /y
+sc stop winmgmt
+timeout 1
+sc start winmgmt
+timeout 1
+exit
